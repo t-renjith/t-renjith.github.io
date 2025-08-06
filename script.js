a7710307acfc276ec941d414f00290473e515e30
Renjith T <renjitht@iisc.ac.in>
2025-08-06 14:43:55 +0530

    update

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ fetch('/footer.html')
     .then(data => {
         document.getElementById('footer').innerHTML = data;
     });
+
+const menuButton = document.getElementById('menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
